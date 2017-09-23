8e143af5ae27b21aa0c2cfde39f5489fbd1630ea
Ben Kremer <ben.kremer@hotmail.co.uk>
2017-09-23 17:48:53 +0200

    Adds basic method docs

--- src/index.js
+++ src/index.js
@@ -10,37 +10,74 @@ const opts = {
   message: undefined,
 };
 
+/**
+ * config - Accepts a config object to adjust default settings.
+ * 
+ * @param {any} userOpts
+ * @returns {void}
+ */
 function config(userOpts) {
   Object.assign(opts, userOpts);
 }
 
+/**
+ * start - Starts a new timer and returns the absolute start time.
+ * 
+ * @returns {number} nsStartTime
+ */
 function start() {
   startTuple = process.hrtime();
   return units.tupleToNano(startTuple);
 }
 
+/**
+ * stop - Stops the current timer and returns the absolute end time.
+ * 
+ * @returns {number} nsEndTime
+ */
 function stop() {
   stopTuple = process.hrtime();
   return units.tupleToNano(stopTuple);
 }
 
-function diff(startTime, endTime) {
-  return endTime - startTime;
+/**
+ * diff - Accepts a start & end time in nanoseconds and returns the difference.
+ * 
+ * @param {number} nsStartTime 
+ * @param {number} nsEndTime 
+ * @returns {number} nsDiff
+ */
+function diff(nsStartTime, nsEndTime) {
+  return nsEndTime - nsStartTime;
 }
 
+/**
+ * stopAndDiff - Combines stop() & diff().
+ * If `opts.shouldPrint` is set, the diff is logged to the console 
+ * with `message` (if specified).
+ * 
+ * @param {string} message Optional message for logging.
+ * @returns {number} nsDiff
+ */
 function stopAndDiff(message) {
   const diffTuple = process.hrtime(startTuple);
-  const diffNano = units.tupleToNano(diffTuple);
+  const nsDiff = units.tupleToNano(diffTuple);
   if (opts.shouldPrint) {
     logger.printDiff(diffTuple, opts.precision, message || opts.message); // Pretty-print result to console
   }
-  return diffNano;
+  return nsDiff;
 }
 
+/**
+ * stopAndRestart - Stops timer, starts a new one and returns the nanosecond
+ * diff of the stopped timer.
+ * 
+ * @returns {number} nsDiff
+ */
 function stopAndRestart() {
-  const diffNano = stop();
+  const nsDiff = stop();
   start();
-  return diffNano;
+  return nsDiff;
 }
 
 const perfTimer = {
@@ -52,4 +89,5 @@ const perfTimer = {
   stopAndDiff,
   stopAndRestart,
 };
+
 module.exports = perfTimer;
